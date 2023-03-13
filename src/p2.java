e6cb57377ce2e086a898f4370db5da6cad313d40
gtmkr1234 <gtmkr1234@gmail.com>
2023-03-13 15:42:10 +0530

    Init

--- src/p2.java
+++ src/p2.java
@@ -4,6 +4,8 @@ public class p2 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         s.useRadix(2);// Used to input Binary Numbers...
+        // Inside this method, the number is the base number of the input :0
+
         int x = s.nextInt();
         System.out.println(x);
     }
