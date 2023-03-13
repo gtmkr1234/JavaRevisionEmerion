c737800674ae12af5c1f3af67098475ee5bd227d
gtmkr1234 <gtmkr1234@gmail.com>
2023-03-13 15:28:38 +0530

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.lang.*;
+import java.util.Scanner;
 class Main{
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(x);
     }
 }
