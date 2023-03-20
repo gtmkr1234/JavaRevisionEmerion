527a3ba3539eccdb0814ad51b70147e95974ad48
gtmkr1234 <gtmkr1234@gmail.com>
2023-03-20 20:48:21 +0530

    Init

--- src/evenodd.java
+++ src/evenodd.java
@@ -4,11 +4,11 @@ public class evenodd {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int a = s.nextInt();
-        if(a%2==0){
-            System.out.println("even");
+        if(a%5==0 && a%11==0){
+            System.out.println("Divisible");
         }
         else{
-            System.out.println("Odd");
+            System.out.println("Not Divisible");
         }
     }
 }
