d1f84b1e70be07eed8b3d5ae650ca75e06b51dec
gtmkr1234 <gtmkr1234@gmail.com>
2023-03-21 14:39:30 +0530

    Init

--- src/triangle.java
+++ src/triangle.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class triangle {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+        if(a+b+c==180){
+            System.out.println("Triangle");
+        }
+        else{
+            System.out.println("Not a Triangle");
+        }
+    }
 }
