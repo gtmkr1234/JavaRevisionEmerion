795c8ed76d4bb05f8e925b567924959b45b24572
gtmkr1234 <gtmkr1234@gmail.com>
2023-03-22 14:30:01 +0530

    Init

---
+++ src/test.java
@@ -0,0 +1,36 @@
+import javax.swing.table.TableCellRenderer;
+
+public class test {
+    public static void main(String[] args) {
+//        int i =11;
+//        i=i++ + ++i;
+//        System.out.println(i);
+//        int a = 11, b=22,c;
+//        c=a+b + a++ + b++ + ++a + ++b;
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        13
+//        24
+//        103
+//    boolean b = true;
+//    b++;
+//        System.out.println(b);
+//        java: bad operand type boolean for unary operator '++'
+//        int a=1, b=2;
+//        System.out.println(--b - ++a+ ++b - --a);
+//        0
+
+//        int i=11;
+//        int j = --(i++);
+//        java: unexpected type
+//        required: variable
+//        found:    value
+
+//        int a=1;
+//        a=a++ + ++a * --a - a--;
+//        System.out.println(a);
+//        5
+
+    }
+}
