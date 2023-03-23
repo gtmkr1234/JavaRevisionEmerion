473405491b779d5280f473b3086b82985db8a2e4
gtmkr1234 <gtmkr1234@gmail.com>
2023-03-23 15:17:10 +0530

    Init

--- src/test3.java
+++ src/test3.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class test3 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        for (int i = 1; i <= 10 ; i++) {
-            System.out.println(n+" x "+i+" = "+n*i);
+//        int n = s.nextInt();
+        for (char i = 'a'; i <= 'z' ; i++) {
+            System.out.println(i);
         }
     }
 }
