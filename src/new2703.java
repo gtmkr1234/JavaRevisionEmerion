7e2499da07c9e5709a210c77016aca66a5ef7e18
gtmkr1234 <gtmkr1234@gmail.com>
2023-03-27 15:16:46 +0530

    Init

--- src/new2703.java
+++ src/new2703.java
@@ -13,13 +13,23 @@ public class new2703 {
 //        System.out.println(res);
 
 
-        //Print the sum of the digits
-        int a = s.nextInt();
-        int res=0;
-        while(a>0){
-            res += a%10;
-            a/=10;
+//        //Print the sum of the digits
+//        int a = s.nextInt();
+//        int res=0;
+//        while(a>0){
+//            res += a%10;
+//            a/=10;
+//        }
+//        System.out.println(res);
+
+        int num = s.nextInt();
+        boolean r = true;
+        for (int i = 2; i < Math.sqrt(num); i++) {
+
+            if(num%i==0) {
+                r = false;
+            }
         }
-        System.out.println(res);
+        System.out.println(r==true?"Prime Number":"Not a prime num");
     }
 }
