7def4b14ad7399aaf6a2950d58f58bcfb287707f
gtmkr1234 <gtmkr1234@gmail.com>
2023-03-27 15:11:21 +0530

    Init

--- src/new2703.java
+++ src/new2703.java
@@ -3,12 +3,22 @@ import java.util.Scanner;
 public class new2703 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        //reverse
+//        //reverse
+//        int a = s.nextInt();
+//        int res =0;
+//        while(a>0){
+//            res = 10*res + a%10;
+//            a = a/10;
+//        }
+//        System.out.println(res);
+
+
+        //Print the sum of the digits
         int a = s.nextInt();
-        int res =0;
+        int res=0;
         while(a>0){
-            res = 10*res + a%10;
-            a = a/10;
+            res += a%10;
+            a/=10;
         }
         System.out.println(res);
     }
