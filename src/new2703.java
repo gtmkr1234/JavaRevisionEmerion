55d94dc4da55b0b5983e75582cee2d6b3a3295a0
gtmkr1234 <gtmkr1234@gmail.com>
2023-03-27 15:09:35 +0530

    Init

---
+++ src/new2703.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class new2703 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        //reverse
+        int a = s.nextInt();
+        int res =0;
+        while(a>0){
+            res = 10*res + a%10;
+            a = a/10;
+        }
+        System.out.println(res);
+    }
+}
--- src/test3.java
+++ src/test3.java
@@ -1,11 +1,27 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class test3 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 //        int n = s.nextInt();
-        for (char i = 'a'; i <= 'z' ; i++) {
-            System.out.println(i);
-        }
-    }
-}
+//        for (char i = 'z'; i >= 'a' ; i--) {
+//            System.out.println(i);
+//        }
+//        int sum=0;
+//        for (int i = 1; i < 101; i++) {
+//            if(i%2!=0)
+//                sum+=i;
+//        }
+//        System.out.println(sum);
+//        for (int i = 1; i <= 255; i++) {
+//            System.out.println((char) i);
+//        }
+//        int a = s.nextInt();
+//        BigInteger res = BigInteger.valueOf(1);
+//        for (int i = 1; i <=a ; i++){
+//            res = res*i;
+//        }
+//        System.out.println(res);
+//    }
+}}
