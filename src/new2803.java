f87a7b6ca64a3905108cf0bcb7c3039fbcd5e574
gtmkr1234 <gtmkr1234@gmail.com>
2023-03-28 15:54:17 +0530

    Init

---
+++ src/new2803.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class new2803 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+//        String a = "java".substring(4);
+//        String a = "java".repeat(4);
+
+//        System.out.println("1".concat("2").repeat(5).charAt(7)); O/P- 2
+//        System.out.println("string".replace('i','0').substring(2,5)); O/P = r0n
+//        System.out.println("Java"==new String("Java")); Ans = False
+//        System.out.println("     Java\tConcept\tOf\tThe\tDay     ".strip()); Ans=> Java	Concept	Of	The	Day
+//        String s1 = "java";
+//        String s2 = s1;
+//        System.out.println(s1==s2);
+//        System.out.println(s1.equals(s2));
+////        System.out.println(s1.hashCode()==s2.hashCode());
+//        true
+//        true
+//        String s1 = new String("JAVA");
+//        String s2 = new String("JAVA");
+//        System.out.println(s1==s2); false
+//        System.out.println(s1.equals(s2)); true
+//        System.out.println(s1.hashCode()==s2.hashCode()); true
+
+//        String s1 = "Java";
+//        String s2 = s1.intern();
+//        String s3 = new String("Java");
+//        System.out.println(s1==s2); true
+//        System.out.println(s3==s2); false
+//        System.out.println(s1==s3);false
+
+
+    }
+}
+//class Str1 extends String{
+//    String name;
+// Can not be extended;
+//}
